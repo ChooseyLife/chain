4ff7566811b1d9317cae1e5d2605815f4dda8404
e <563798627@qq.com>
2022-12-03 14:30:25 +0800

    feat: 完成链接钱包方法,并初始化合同, 设置首页Mint按钮

--- full-mint-website/src/App.js
+++ full-mint-website/src/App.js
@@ -1,8 +1,28 @@
+import { connectWalletHandler } from "./utils/connect";
+import { Text, Button } from "@chakra-ui/react";
 import './App.css';
 
 function App() {
+  const isConnect = connectWalletHandler();
   return (
-    <div className="App"></div>
+    <div className="App">
+      {
+        isConnect 
+        ? <Button
+        backgroundColor="#D6517D"
+        borderRadius="5px"
+        boxShadow="0px 2px 2px 1px #0F0F0F"
+        color="white"
+        cursor="pointer"
+        fontFamily="inherit"
+        padding="15px"
+        marginTop="10px"
+      >
+        Mint Now
+      </Button>
+      : <Text>You must be connected to Mint.</Text>
+      }
+    </div>
   );
 }
 
---
+++ full-mint-website/src/utils/connect.js
@@ -0,0 +1,31 @@
+import { ethers } from "ethers";
+import abi from "../assets/RoboPunksNFT.json"
+
+export const connectWalletHandler = async () => {
+  if (!window.ethereum) {
+    message.error("please install wallet!");
+    return "";
+  }
+
+  try {
+    const accounts = await window.ethereum.request({
+      method: "eth_requestAccounts",
+    });
+    console.log("Found an account! Address: ", accounts[0]);
+    return accounts[0];
+  } catch (err) {
+    console.error(err);
+    return "";
+  }
+};
+
+export const contractInstance = () => {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(
+    process.env.REACT_APP_CONTRACT_ADDRESS, // 合约地址
+    abi,
+    signer
+  );
+  return contract;
+}
