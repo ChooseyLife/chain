fd25139d2017323d8d5471ba61bef158996d5435
e <563798627@qq.com>
2023-04-13 16:13:03 +0800

    feat: NFT合约代码提交

---
+++ NFT-Collection/hardhat-tutorial/constants/index.js
@@ -0,0 +1,7 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xD4160e39b6945bc709c44Cea171f510a29Ef0CFd";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+const NFT_ADDRESS = "0xb4F77d61072d86EAb8614e0E1500ff0082A28c96"
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
